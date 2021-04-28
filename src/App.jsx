71efe6d426c8f52937a5d79aa91d4f5eb100dec9
levkotman <leoktmn@yandex.ru>
2021-04-29 00:48:20 +0200

    removed rows for debug, added get request functionality

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,27 @@ function App() {
   const [id, setId] = useState([]);
   const [registryDate, setRegistryDate] = useState([]);
   const [lastActivityDate, setLastActivityDate] = useState([]);
+  const [usersList, setUsersList] = useState([]);
 
   const addUser = () => {
     Axios.post("http://localhost:3001/create", {
       id: id,
       registry_date: registryDate,
       last_activity_date: lastActivityDate,
-    }).then(() => {
-      console.log("axios post success");
+    })
+      .then(() => {
+        console.log("axios post success");
+        document.getElementById("#users").innerHTML = "users infos";
+      })
+      .catch(() => {
+        alert(
+          "Вы ввели данные неверно, попробуйте проверить и отправить еще раз"
+        );
+      });
+  };
+  const getUsers = () => {
+    Axios.get("http://localhost:3001/users").then((response) => {
+      console.log(response);
     });
   };
 
@@ -99,117 +112,18 @@ function App() {
               />
             </td>
           </tr>
-          <tr>
-            <td>
-              <input
-                type="number"
-                min="0"
-                name=""
-                id=""
-                onBlur={(event) => {
-                  setId((id) => [...id, event.target.value]);
-                }}
-              />
-            </td>
-            <td>
-              <input
-                type="date"
-                onBlur={(event) => {
-                  setRegistryDate((registryDate) => [
-                    ...registryDate,
-                    event.target.value,
-                  ]);
-                }}
-              />
-            </td>
-            <td>
-              <input
-                type="date"
-                onBlur={(event) => {
-                  setLastActivityDate((lastActivityDate) => [
-                    ...lastActivityDate,
-                    event.target.value,
-                  ]);
-                }}
-              />
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <input
-                type="number"
-                min="0"
-                name=""
-                id=""
-                onBlur={(event) => {
-                  setId((id) => [...id, event.target.value]);
-                }}
-              />
-            </td>
-            <td>
-              <input
-                type="date"
-                onBlur={(event) => {
-                  setRegistryDate((registryDate) => [
-                    ...registryDate,
-                    event.target.value,
-                  ]);
-                }}
-              />
-            </td>
-            <td>
-              <input
-                type="date"
-                onBlur={(event) => {
-                  setLastActivityDate((lastActivityDate) => [
-                    ...lastActivityDate,
-                    event.target.value,
-                  ]);
-                }}
-              />
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <input
-                type="number"
-                min="0"
-                name=""
-                id=""
-                onBlur={(event) => {
-                  setId((id) => [...id, event.target.value]);
-                }}
-              />
-            </td>
-            <td>
-              <input
-                type="date"
-                onBlur={(event) => {
-                  setRegistryDate((registryDate) => [
-                    ...registryDate,
-                    event.target.value,
-                  ]);
-                }}
-              />
-            </td>
-            <td>
-              <input
-                type="date"
-                onBlur={(event) => {
-                  setLastActivityDate((lastActivityDate) => [
-                    ...lastActivityDate,
-                    event.target.value,
-                  ]);
-                }}
-              />
-            </td>
-          </tr>
         </tbody>
       </table>
       <button type="submit" onClick={addUser}>
         Save
       </button>
-      <button type="reset">Clear</button>
+      <button type="button" onClick={getUsers}>
+        Show users
+      </button>
+      <section>
+        <h1>Users:</h1>
+        <p id="users"></p>
+      </section>
     </div>
   );
 }
