0c24e6c186edf7604838faad7d349caad42c9b2b
levkotman <leoktmn@yandex.ru>
2021-04-29 23:23:47 +0200

    response dates to arr, fixed button bug

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ function App() {
   const [registryDate, setRegistryDate] = useState([]);
   const [lastActivityDate, setLastActivityDate] = useState([]);
   const [usersList, setUsersList] = useState([]);
+  let rollingRetentionShown = false;
 
   const addUser = () => {
     Axios.post("http://localhost:3001/create", {
@@ -16,9 +17,13 @@ function App() {
     })
       .then(() => {
         console.log("axios post success");
-        setUsersList([...usersList, {id: id,
-          registry_date: registryDate,
-          last_activity_date: lastActivityDate},
+        setUsersList([
+          ...usersList,
+          {
+            id: id,
+            registry_date: registryDate,
+            last_activity_date: lastActivityDate,
+          },
         ]);
       })
       .catch(() => {
@@ -33,38 +38,44 @@ function App() {
       setUsersList(response.data);
     });
   };
+  let usersObjectsArr = usersList.map((value, key) => {
+    return {
+      id: value.id,
+      registryDate: value.registry_date,
+      lastActivityDate: value.last_activity_date,
+    };
+  });
 
   function dateDiffinDays(date2, date1) {
-    debugger;
-    let time2 = date2.getTime();
-    let time1 = date1.getTime();
+    let time2 = new Date(date2).getTime();
+    let time1 = new Date(date1).getTime();
 
     return parseInt((time2 - time1) / (24 * 3600 * 1000));
   }
 
-  function rollingRetention7days (users = {lastActivityDate, registryDate}) {
-    debugger;
+  function rollingRetention7days(users = {lastActivityDate, registryDate}) {
     //Rolling Retention X day = (количество пользователей, вернувшихся в систему в X-ый день или позже) / (количество пользователей, установивших приложение X дней назад или раньше) * 100%.
-    let now = new Date();
+
     //фильтр списка пользователей числителя
-    let activeUsersArr = users.filter(user => {
+    let activeUsersArr = users.filter((user) => {
       return dateDiffinDays(user.lastActivityDate, user.registryDate) >= 7;
     });
-    console.log('пользователи зашли спустя более 7 дней' + activeUsersArr);
+    console.log(activeUsersArr);
     // фильтр пользователей знаменателя
-    let old7daysUsersArr = users.filter(user => {
-      return dateDiffinDays(user.registryDate, now) >= 7;
+    let old7daysUsersArr = users.filter((user) => {
+      return dateDiffinDays(user.registryDate, new Date()) >= 7;
     });
-    console.log('пользователи регистрировались больше 7 дней назад' + old7daysUsersArr);
+    console.log(old7daysUsersArr);
 
     // формула с проверкой на 0
     if (old7daysUsersArr.length === 0) {
+      rollingRetentionShown = true;
       return "В базе данных нет пользователей с RR 7 дней";
     } else {
+      rollingRetentionShown = true;
       return (activeUsersArr.length / old7daysUsersArr.length) * 100;
     }
-  };
-  // let renderRR = rollingRetention7days(usersList);
+  }
 
   return (
     <div className="App">
@@ -158,22 +169,23 @@ function App() {
       </button>
       <section>
         <h1>Users:</h1>
-          {usersList.map((value, key) => {
-            return (
-              <p id="users">
-                  <span>UserID: {value.id}</span>
-                  <span>
-                    Date of Registration: {value.registry_date}
-                  </span>
-                  <span>
-                    Last activity date: {value.last_activity_date}
-                  </span>
-                </p>
-            );
-          })}
+        {usersList.map((value, key) => {
+          return (
+            <p id="users">
+              <span>UserID: {value.id}</span>
+              <span>Date of Registration: {value.registry_date}</span>
+              <span>Last activity date: {value.last_activity_date}</span>
+            </p>
+          );
+        })}
       </section>
-        <button type="button" onClick={rollingRetention7days(usersList)}>Calculate Rolling Retention</button>
-        <p>{rollingRetention7days(usersList)}</p>
+      <button
+        type="button"
+        onClick={() => rollingRetention7days(usersObjectsArr)}
+      >
+        Calculate Rolling Retention
+      </button>
+      <p>{rollingRetentionShown && rollingRetention7days(usersObjectsArr)}</p>
     </div>
   );
 }
